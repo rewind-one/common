a346b86a44e571587e0602d28f3364e5210f0a5c
karajan <scisaga@gmail.com>
2018-05-04 18:27:12 +0800

    增加测试方法

--- src/test/java/one/rewind/io/docker/test/DockerHostTest.java
+++ src/test/java/one/rewind/io/docker/test/DockerHostTest.java
@@ -1,4 +1,18 @@
 package one.rewind.io.docker.test;
 
 public class DockerHostTest {
+
+	/**
+	 *
+	 */
+	public void testCreateContainer() {
+
+		// 1. 创建 Host
+
+		// 2. 创建 Container
+
+		// 3. 测试执行命令
+
+		// 4.
+	}
 }
