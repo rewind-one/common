75adcf0f839b9d54b95fb5670d46e6bbed4b27e2
karajan <scisaga@gmail.com>
2018-05-31 18:45:32 +0800

    update code

--- src/main/java/one/rewind/io/requester/chrome/action/LoginWithGeetestAction.java
+++ src/main/java/one/rewind/io/requester/chrome/action/LoginWithGeetestAction.java
@@ -10,6 +10,17 @@ public class LoginWithGeetestAction extends LoginAction {
 
 	GeetestAction action;
 
+	public String geetestContentCssPath = ".geetest_radar_tip";
+
+	public String geetestWindowCssPath = ".geetest_window";
+	public String geetestSliderButtonCssPath = ".geetest_slider_button";
+	public String geetestSuccessMsgCssPath = ".geetest_success_radar_tip_content";
+
+	public String geetestResetTipCssPath = "#password-captcha-box > div.geetest_holder.geetest_wind.geetest_radar_error > div.geetest_btn > div.geetest_radar_btn > div.geetest_radar_tip > span.geetest_reset_tip_content";
+	public String geetestRefreshButtonCssPath = "a.geetest_refresh_1";
+	//#password-captcha-box > div.geetest_holder.geetest_wind.geetest_radar_error > div.geetest_btn > div.geetest_radar_btn > div.geetest_radar_tip > span.geetest_radar_error_code
+	public String geetestRefreshTooManyErrorCssPath = "span.geetest_radar_error_code";
+
 	public LoginWithGeetestAction() {}
 
 	public LoginWithGeetestAction(Account account) {
@@ -21,6 +32,12 @@ public class LoginWithGeetestAction extends LoginAction {
 		if(!fillUsernameAndPassword()) return;
 
 		action = new GeetestAction();
+		action.geetestContentCssPath = geetestContentCssPath;
+		action.geetestWindowCssPath = geetestWindowCssPath;
+		action.geetestSliderButtonCssPath = geetestSliderButtonCssPath;
+		action.geetestSuccessMsgCssPath = geetestSuccessMsgCssPath;
+		action.geetestResetTipCssPath = geetestResetTipCssPath;
+		action.geetestRefreshButtonCssPath = geetestRefreshButtonCssPath;
 		action.run();
 
 		if(clickSubmitButton()) {
