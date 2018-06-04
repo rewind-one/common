ad66b5689c7c863027f5ee565f5548b9315ff1db
karajan <scisaga@gmail.com>
2018-06-04 12:07:55 +0800

    update code

--- src/test/java/one/rewind/opencv/test/OpenCVTest.java
+++ src/test/java/one/rewind/opencv/test/OpenCVTest.java
@@ -9,6 +9,9 @@ import org.opencv.core.MatOfByte;
 import org.opencv.core.Rect;
 import org.opencv.imgcodecs.Imgcodecs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OpenCVTest {
 
 	@Test
@@ -30,11 +33,71 @@ public class OpenCVTest {
 
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-		String path = "tmp/cellphone.png";
+		String path = "tmp/1_8.jpg";
 
-		Mat mat = Imgcodecs.imread(path);
 
-		int x_max = mat.cols();
+
+		// 灰度模式读取
+		Mat mat = Imgcodecs.imread(path, Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+
+		List<Integer> h_line_positions = new ArrayList<>();
+
+		for (int w = 0; w < mat.width(); w++) {
+
+			boolean h_line = true;
+			for (int h = 0; h < mat.height(); h++) {
+
+				double[] pixel = mat.get(h, w);
+				h_line = h_line && (pixel[0] > 205);
+			}
+
+			if(h_line) {
+
+				h_line_positions.add(w);
+
+				for(int h=0; h<mat.height(); h++) {
+
+					mat.put(h, w,128);
+				}
+			}
+		}
+
+		/**
+		 *
+		 */
+		for(int i=0; i<h_line_positions.size(); i++) {
+
+			if(i == h_line_positions.size() - 2 && ( mat.width() - h_line_positions.get(i) ) < 3) {
+				return;
+			}
+			else if(i == h_line_positions.size() - 1) {
+				return;
+			}
+			else {
+
+				int x = h_line_positions.get(i) + 1;
+				int y = 0;
+				int width = h_line_positions.get(i+1) - x;
+				int height = mat.height();
+
+				Rect rectCrop = new Rect(x, y, width, height);
+				Mat image_roi = new Mat(mat, rectCrop);
+
+				MatOfByte mob = new MatOfByte();
+				Imgcodecs.imencode(".jpg", image_roi, mob);
+				byte ba[] = mob.toArray();
+
+				FileUtil.writeBytesToFile(ba, "tmp/1_8_" + x + ".jpg");
+			}
+		}
+/*
+		MatOfByte mob = new MatOfByte();
+		Imgcodecs.imencode(".jpg", mat, mob);
+		byte ba[] = mob.toArray();
+
+		FileUtil.writeBytesToFile(ba, "tmp/1_8_.jpg");*/
+
+		/*int x_max = mat.cols();
 
 		int x = 0;
 		int y = 3;
@@ -56,7 +119,7 @@ public class OpenCVTest {
 
 			i++;
 			x = x + 6 + (i / 4);
-		}
+		}*/
 	}
 
 }
